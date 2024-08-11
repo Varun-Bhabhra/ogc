8747119e57a77538d2ff625f11a99b3a6a8fd48a
“varun-bhabhra” <bhabhravarun@gmail.com>
2024-08-11 17:47:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section>
+      <h1 className="text-7xl bg-green-600">Hello</h1>
+    </section>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#19B4B1",
+        secondary: "#5AB87E",
+        dark: "#131313",
+        light: "#ffffff",
+        extraOne: "#7D26C7",
+        extraTwo: "#FC0182",
+      },
+      fontFamily: {
+        metro: ["metropolis", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
