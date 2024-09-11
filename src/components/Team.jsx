36cc1ba699919c96b4fbc6ff80c4704c3a64ce62
“varun-bhabhra” <bhabhravarun@gmail.com>
2024-09-12 01:00:20 +0530

    test4

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -57,27 +57,6 @@ const Team = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-        {/* <div className="grid grid-cols-2 gap-x-4 gap-y-6 sm:gap-y-8 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
-          <div className="flex flex-col items-center gap-2 sm:flex-row md:gap-4">
-            <div className="h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:h-32 md:w-32">
-              <img
-                src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
-                loading="lazy"
-                alt="Photo by Radu Florin"
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-
-            <div>
-              <div className="text-center font-bold text-indigo-500 sm:text-left md:text-lg">
-                John McCulling
-              </div>
-              <p className="text-center text-sm text-gray-500 sm:text-left md:text-base">
-                Founder / CEO
-              </p>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
